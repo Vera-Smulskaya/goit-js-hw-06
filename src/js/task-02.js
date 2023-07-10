a089d6ee601df43b6ee17476db381512356bcfc5
Vira Smulska <95082786+Vera-Smulskaya@users.noreply.github.com>
2023-07-10 21:39:16 +0300

    done task-02.js

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -1,8 +1,18 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const listElements = ingredients.map((item) => {
+  const listElement = document.createElement("li");
+  listElement.textContent = item;
+  listElement.classList.add("item");
+  return listElement;
+});
+
+const listIngredients = document.querySelector("#ingredients");
+listIngredients.append(...listElements);
