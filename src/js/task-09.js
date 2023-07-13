4fbd1a30f86bd29ea325485f3a1fa7dee8cb28b2
Vira Smulska <95082786+Vera-Smulskaya@users.noreply.github.com>
2023-07-13 23:01:28 +0300

    fix task-09.js

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -3,3 +3,14 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const spanChangeColor = document.querySelector(".color");
+const btnChangeColor = document.querySelector(".change-color");
+
+btnChangeColor.addEventListener("click", onClickChangeColor);
+
+function onClickChangeColor() {
+  let randomColor = getRandomHexColor();
+  document.body.style.backgroundColor = `${randomColor}`;
+  spanChangeColor.textContent = randomColor;
+}
