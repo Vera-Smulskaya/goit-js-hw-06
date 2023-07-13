949f8d1d9dc0ff54f50797df44d134e185e18ff3
Vira Smulska <95082786+Vera-Smulskaya@users.noreply.github.com>
2023-07-13 22:42:48 +0300

    fix task-08.js

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,22 @@
+const form = document.querySelector(".login-form");
 
+form.addEventListener("submit", onFormSubmit);
+
+function onFormSubmit(event) {
+  event.preventDefault();
+
+  const { email, password } = event.currentTarget.elements;
+
+  if (!email.value || !password.value) {
+    alert("fill in all form's fields");
+    return;
+  }
+
+  const formData = {
+    email: email.value,
+    password: password.value,
+  };
+
+  console.log("formData:", formData);
+  event.currentTarget.reset();
+}
