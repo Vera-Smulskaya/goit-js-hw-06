2a9ab8869d261663ffd3518d6bc5d020fca66215
Vira Smulska <95082786+Vera-Smulskaya@users.noreply.github.com>
2023-07-16 12:33:27 +0300

    add comments about tasks

--- js/task-01.js
+++ js/task-01.js
@@ -1,3 +1,7 @@
+// Напиши скрипт, який:
+// Порахує і виведе в консоль кількість категорій в ul#categories, тобто елементів li.item.
+// Для кожного элемента li.item у списку ul#categories, знайде і виведе в консоль текст заголовку елемента (тегу <h2>) і кількість елементів в категорії (усіх <li>, вкладених в нього).
+
 const listCategories = document.querySelectorAll("#categories .item");
 const numberListCategories = listCategories.length;
 console.log("Number of categories: ", numberListCategories);
--- js/task-07.js
+++ js/task-07.js
@@ -1,3 +1,5 @@
+// Напиши скрипт, який реагує на зміну значення input#font-size-control (подія input) і змінює інлайн-стиль span#text, оновлюючи властивість font-size. В результаті, перетягуючи повзунок, буде змінюватися розмір тексту.
+
 const input = document.getElementById("font-size-control");
 const span = document.getElementById("text");
 
